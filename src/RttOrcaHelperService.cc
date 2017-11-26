e4344096d58f6c57f5f28e4a0111f6e31f1b6614
Antoine Hoarau <hoarau.robotics@gmail.com>
2017-11-26 13:10:04 +0100

    inserting/removing all working

--- src/RttOrcaHelperService.cc
+++ src/RttOrcaHelperService.cc
@@ -19,8 +19,14 @@ namespace util
         : RTT::Service("orca_helper",owner)
         , owner_(owner)
         {
+            if(owner_)
+            {
+                RTT::log(RTT::Info) << "RttOrcaRegister created for task " << owner->getName() << RTT::endlog();
+            }
             this->addOperation("activateAll", &RttOrcaRegister::activateAll, this, RTT::ClientThread);
             this->addOperation("desactivateAll", &RttOrcaRegister::desactivateAll, this, RTT::ClientThread);
+            this->addOperation("insertAllInProblem", &RttOrcaRegister::insertAllInProblem, this, RTT::ClientThread);
+            this->addOperation("removeAllFromProblem", &RttOrcaRegister::removeAllFromProblem, this, RTT::ClientThread);
             this->addOperation("printAll", &RttOrcaRegister::printAll, this, RTT::ClientThread);
         }
         
@@ -39,6 +45,37 @@ namespace util
             {
                 RTT::log(RTT::Info) << "Desactivating " << t->getName() << RTT::endlog();
                 t->desactivate();
+            }
+        }
+        
+        void insertAllInProblem()
+        {
+            for(auto t : orca::optim::OptimisationVector().getAllCreatedObjects())
+            {
+                RTT::log(RTT::Info) << "Inserting " << t->getName() <<  " in problem" << RTT::endlog();
+                t->insertInProblem();
+            }
+        }
+        
+        void removeAllFromProblem()
+        {
+            if(!getOwner())
+            {
+                return;
+            }
+
+            for(auto pname : getOwner()->getPeerList())
+            {
+                if( getOwner()->getPeer(pname)->provides()->hasOperation("removeFromProblem") )
+                {
+                    RTT::log(RTT::Info) << "Removing peer " << pname << " from problem"  << RTT::endlog();
+                    RTT::OperationCaller<bool()> peer_function(getOwner()->getPeer(pname)->getOperation("removeFromProblem"), getOwner()->getPeer(pname)->engine());
+                    if (!peer_function.ready()) continue ;
+                    if(peer_function())
+                    {
+                        RTT::log(RTT::Info) << "Peer " << pname << " successfully removed from problem"  << RTT::endlog();
+                    }
+                }
             }
         }
         
